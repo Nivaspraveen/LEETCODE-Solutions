311ecb0be983a5e12a1ca7f8fe91d5bdeb560f28
nivas_pravin75 <51258356+Nivaspraveen@users.noreply.github.com>
2024-11-07 19:49:11 +0530

    Time: 1191 ms (12.65%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1182-game-play-analysis-iv/game-play-analysis-iv.sql
@@ -0,0 +1,10 @@
+# Write your MySQL query statement below
+WITH FirstLogin AS (
+    SELECT player_id, MIN(event_date) AS first_login_date FROM Activity GROUP BY player_id
+),
+ConsecutiveLogins AS (
+    SELECT f.player_id FROM FirstLogin f JOIN Activity a
+    ON f.player_id = a.player_id AND a.event_date = DATE_ADD(f.first_login_date, INTERVAL 1 DAY)
+)
+SELECT ROUND(COUNT(DISTINCT cl.player_id) / COUNT(DISTINCT f.player_id), 2) AS fraction
+FROM FirstLogin f LEFT JOIN ConsecutiveLogins cl ON f.player_id = cl.player_id;
