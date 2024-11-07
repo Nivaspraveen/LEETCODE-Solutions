0cb1e4167062c5de64f58faea52beaad7371ca4f
nivas_pravin75 <51258356+Nivaspraveen@users.noreply.github.com>
2024-11-07 19:27:14 +0530

    Time: 0 ms (100.00%) | Memory: 52.3 MB (6.64%) - LeetSync

---
+++ 101-symmetric-tree/symmetric-tree.js
@@ -0,0 +1,22 @@
+/**
+ * Definition for a binary tree node.
+ * function TreeNode(val, left, right) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.left = (left===undefined ? null : left)
+ *     this.right = (right===undefined ? null : right)
+ * }
+ */
+/**
+ * @param {TreeNode} root
+ * @return {boolean}
+ */
+
+ // Recursive Approach
+var isSymmetric = function(root) {
+    const isMirror = (left, right) => {
+        if (left === null && right === null) return true;
+        if (left === null || right === null || left.val !== right.val) return false;
+        return isMirror(left.left, right.right) && isMirror(left.right, right.left);
+    }
+    return isMirror(root.left, root.right);
+};
