42b1618ee9261c5fe6078d7e7cf6e7b1fe57064c
nivas_pravin75 <51258356+Nivaspraveen@users.noreply.github.com>
2024-11-11 11:27:44 +0530

    Time: 0 ms (100.00%) | Memory: 51.8 MB (33.75%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.js
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.js
@@ -3,11 +3,14 @@
  * @return {number[]}
  */
 var runningSum = function(nums) {
-    const runningSum = [];
-    let currSum = 0;
-    for (let i = 0; i < nums.length; i++) {
-        currSum += nums[i];
-        runningSum.push(currSum);
-    }
-    return runningSum;
+    // const runningSum = [];
+    // let currSum = 0;
+    // for (let i = 0; i < nums.length; i++) {
+    //     currSum += nums[i];
+    //     runningSum.push(currSum);
+    // }
+    // return runningSum;
+
+    let cumulativeSum = 0;
+    return nums.map(num => cumulativeSum += num);
 };
