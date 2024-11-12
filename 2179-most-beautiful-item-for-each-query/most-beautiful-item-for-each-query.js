d8f7feca3eb8a5e7dfac2ba2a017ac92643410be
nivas_pravin75 <51258356+Nivaspraveen@users.noreply.github.com>
2024-11-12 13:54:16 +0530

    Time: 134 ms (50.00%) | Memory: 89.7 MB (14.29%) - LeetSync

--- 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.js
+++ 2179-most-beautiful-item-for-each-query/most-beautiful-item-for-each-query.js
@@ -5,15 +5,14 @@
  */
 var maximumBeauty = function(items, queries) {
     items.sort((a, b) => a[0] - b[0]);
-    let sortedQueries = queries.map((q, i) => [q, i]);
-    sortedQueries.sort((a, b) => a[0] - b[0]);
-    let result = new Array(queries.length).fill(0), maxBeauty = 0, itemIndex = 0;
-    for (let [query, originalIndex] of sortedQueries) {
-        while (itemIndex < items.length && items[itemIndex][0] <= query) {
-            maxBeauty = Math.max(maxBeauty, items[itemIndex][1]);
-            itemIndex++;
+    const sortedQueries = queries.map((q, i) => [q, i]).sort((a, b) => a[0] - b[0]), results = Array(queries.length).fill(0);
+    let maxBeauty = 0, j = 0;
+    for (const [query, originalIndex] of sortedQueries) {
+        while (j < items.length && items[j][0] <= query) {
+            maxBeauty = Math.max(maxBeauty, items[j][1]);
+            j++;
         }
-        result[originalIndex] = maxBeauty;
+        results[originalIndex] = maxBeauty;
     }
-    return result;
+    return results;
 };
