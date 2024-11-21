221a059698c6536afc8fb6d52daa6fcefabac3c2
nivas_pravin75 <51258356+Nivaspraveen@users.noreply.github.com>
2024-11-21 09:46:32 +0530

    Time: 164 ms (33.33%) | Memory: 81.8 MB (42.86%) - LeetSync

--- 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.js
+++ 2343-count-unguarded-cells-in-the-grid/count-unguarded-cells-in-the-grid.js
@@ -6,19 +6,24 @@
  * @return {number}
  */
 var countUnguarded = function(m, n, guards, walls) {
-    const occupied = new Set(), guarded = new Set(), toKey = (x, y) => `${x},${y}`;
-    for (const [x, y] of guards) occupied.add(toKey(x, y));
-    for (const [x, y] of walls) occupied.add(toKey(x, y));
+    const grid = Array.from({length: m}, () => Array(n).fill(0)), guarded = 1, occupied = 2;
+    for (const [x, y] of guards) grid[x][y] = occupied;
+    for (const [x, y] of walls) grid[x][y] = occupied;
     for (const [x, y] of guards) {
         for (const [dx, dy] of [[0, 1], [1, 0], [0, -1], [-1, 0]]) {
             let nx = x + dx, ny = y + dy;
-            while (nx >= 0 && ny >= 0 && nx < m && ny < n && !occupied.has(toKey(nx, ny))) {
-                guarded.add(toKey(nx, ny));
+            while (nx >= 0 && ny >= 0 && nx < m && ny < n && grid[nx][ny] !== occupied) {
+                if (grid[nx][ny] === 0) grid[nx][ny] = guarded;
                 nx += dx;
                 ny += dy;
             }
         }
     }
-    const totalCells = m * n, unoccupied = totalCells - occupied.size;
-    return unoccupied - guarded.size;
+    let unguarded = 0;
+    for (let i = 0; i < m; i++) {
+        for (let j = 0; j < n; j++) {
+            if (grid[i][j] === 0) unguarded++;
+        }
+    }
+    return unguarded;
 };
