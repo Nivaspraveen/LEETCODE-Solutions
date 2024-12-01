94ee691c5b353a3a06d9edf914edafcfb12b899f
nivas_pravin75 <51258356+Nivaspraveen@users.noreply.github.com>
2024-12-01 10:24:44 +0530

    Time: 0 ms (100.00%) | Memory: 50.6 MB (40.03%) - LeetSync

---
+++ 1468-check-if-n-and-its-double-exist/check-if-n-and-its-double-exist.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var checkIfExist = function(arr) {
+    const seen = new Set();
+    for (let i = 0; i < arr.length; i++) {
+        if (seen.has(arr[i] * 2) || arr[i] % 2 === 0 && seen.has(arr[i] / 2)) return true;
+        seen.add(arr[i]);
+    }
+    return false;
+};
