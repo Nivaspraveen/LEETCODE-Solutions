e1ac80f4395066965bf1250929aeff38cfe1f090
nivas_pravin75 <51258356+Nivaspraveen@users.noreply.github.com>
2024-12-09 12:14:49 +0530

    Time: 16 ms (23.08%) | Memory: 85 MB (46.67%) - LeetSync

--- 3427-special-array-ii/special-array-ii.js
+++ 3427-special-array-ii/special-array-ii.js
@@ -5,14 +5,13 @@
  */
 var isArraySpecial = function(nums, queries) {
     const n = nums.length, prefixSum = new Array(n).fill(0), result = [];
-
     for (let i = 1; i < n; i++) {
         prefixSum[i] = prefixSum[i - 1];
         if (nums[i] % 2 === nums[i - 1] % 2) prefixSum[i]++;
     }
     for (const query of queries) {
-        const [start, end] = query;
-        const falseCount = prefixSum[end] - (start > 0 ? prefixSum[start] : 0);
+        const [start, end] = query,
+            falseCount = prefixSum[end] - (start > 0 ? prefixSum[start] : 0);
         result.push(falseCount && start !== end ? false : true)
     }
     return result;    
