c7c2c3c982f5bb3dcc6ae2661cd7832a8e93bf8c
nivas_pravin75 <51258356+Nivaspraveen@users.noreply.github.com>
2024-12-19 09:06:37 +0530

    Time: 0 ms (100.00%) | Memory: 49.3 MB (6.45%) - LeetSync

--- 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.js
+++ 780-max-chunks-to-make-sorted/max-chunks-to-make-sorted.js
@@ -5,7 +5,7 @@
 var maxChunksToSorted = function(arr) {
     let m = 0, c = 0;
     for (let i = 0; i < arr.length; i++) {
-        m = Math.max(m, arr[i]);
+        if (arr[i] > m) m = arr[i];
         if (m === i) c++;
     }
     return c;
